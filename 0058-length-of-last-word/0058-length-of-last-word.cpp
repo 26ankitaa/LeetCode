8abec6bb73abf86821a85769ad177e32dcaecad2
Ankita Kumari <122377058+26ankitaa@users.noreply.github.com>
2024-04-01 10:27:20 +0530

    Time: 4 ms (47.87%), Space: 7.6 MB (57.50%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.length();
-        int i = n-1;
-        
-        while(i>=0 && s[i]== ' ')
-        {
-            i--;
-        }
-        
-        int length = 0;
-        
-        while(i>=0 && s[i] !=' ')
-        {
-            i--;
-            length++;
-        }
-        
-        return length;
+        int idx = s.find_last_not_of(' ')+1;
+        s.erase(idx);
+        int last_space_idx = s.find_last_of(' ');
+        return last_space_idx == string::npos?s.length():s.length()-last_space_idx-1;
     }
 };

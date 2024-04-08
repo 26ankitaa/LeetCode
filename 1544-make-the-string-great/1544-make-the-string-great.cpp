20c48c7837f072c39f8f138a6e633fc333b2640d
Ankita Kumari <122377058+26ankitaa@users.noreply.github.com>
2024-04-08 10:06:06 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (71.82%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     string makeGood(string s) {
+        string result = "";
         
-        string result= string(1,s[0]);;
-        int l=s.size();
-        for(int i=1;i<l;i++){
-            if(!result.empty() && (result.back() + 32==s[i]|| 
-            result.back() - 32==s[i])){
+        for(char &ch:s)
+        {
+            if(result.size() > 0 && (ch-32==result.back() || ch+32==result.back()))
+            {
                 result.pop_back();
             }
-            else{
-                result.push_back(s[i]);
+            else
+            {
+                result.push_back(ch);
             }
         }
+        
         return result;
     }
 };
